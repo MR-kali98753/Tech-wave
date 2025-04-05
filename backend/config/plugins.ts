3d73d58a452fe4c420545ca899dcd34dbed4d50f
MR kali <mrkali98753@gmail.com>
2025-04-05 10:51:49 +0530

    fixed backend/config/plugin.ts

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,7 +1,12 @@
-import { env } from "process";
-
-export default () => ({env}) => {
+interface Env {
+    (key: string): string;
+    int: (key: string, defaultValue?: number) => number;
+    bool: (key: string, defaultValue?: boolean) => boolean;
+  }
+  
+  export default ({ env }: { env: Env }) => ({
     ckeditor5: {
-        enabled: true
-    }
-}
+      enabled: true,
+    },
+  });
+  
